eb90e06a3819f1cffc3981ed55c2f95fb08cb945
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-21 01:53:41 +0800

    >  sim RTL ysyx_22040947 唐承捷 Linux TCJ-R7000 5.10.60.1-microsoft-standard-WSL2 #1 SMP Wed Aug 25 23:20:18 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:53:41 up 14:54,  0 users,  load average: 0.11, 0.08, 0.05

--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -45,7 +45,7 @@ VM_USER_CLASSES = \
 
 # User .cpp directories (from .cpp's on Verilator command line)
 VM_USER_DIR = \
-	./test_our \
+	./switch \
 
 
 ### Default rules...
@@ -57,7 +57,7 @@ include $(VERILATOR_ROOT)/include/verilated.mk
 ### Executable rules... (from --exe)
 VPATH += $(VM_USER_DIR)
 
-switch_sim.o: ./test_our/switch_sim.cpp
+switch_sim.o: ./switch/switch_sim.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 
 ### Link rules... (from --exe)
